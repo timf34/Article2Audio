b7f42c6f904e279d6e43bb92004dba0d86058d38
timf34 <farrelti@tcd.ie>
2024-05-01 23:54:18 +0100

    updated config.py with .env

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
-OPENAI_KEY: str = "key"
+"""
+Create a .env file in the root directory of the project and add the following line:
+OPENAI_KEY=your_openai_key
+"""
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_KEY = os.getenv('OPENAI_KEY')
+
