c2105b024e37e102c6fcc6e11688b539a14bf480
timf34 <farrelti@tcd.ie>
2024-05-01 23:37:42 +0100

    added Development mode

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 
 client = OpenAI(api_key=OPENAI_KEY)
 
+DEVELOPMENT: bool = True
+
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -22,21 +25,20 @@ def login():
 def home():
     if request.method == 'POST':
         url = request.form['url']
-        # scraper = substack.SubstackScraper()
-        # text = scraper.get_post_content(url)
-        text = "what can we expect from friendship? \
-                    [pause]\
-                    I will arise and go now, and go to inisfree"
-        response = client.audio.speech.create(
-            model="tts-1",
-            voice="alloy",
-            input=text
-        )
-        audio_data = BytesIO(response.content)
-        # with open("speech.mp3", "rb") as file:
-        #     audio_data = file.read()
+        if DEVELOPMENT:
+            audio_data = "speech.mp3"
+        else:
+            scraper = substack.SubstackScraper()
+            text = scraper.get_post_content(url)
+            response = client.audio.speech.create(
+                model="tts-1",
+                voice="alloy",
+                input=text
+            )
+            audio_data = BytesIO(response.content)
         return send_file(audio_data, mimetype='audio/mpeg', as_attachment=True, download_name='audio.mp3')
     return render_template('home.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
