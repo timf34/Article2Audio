a191f9be593aa27e6f945c72cba955cb69d8891e
timf34 <farrelti@tcd.ie>
2024-05-06 23:22:37 +0100

    working, let's see if it works on vercel

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 import logging
-
-from flask import Flask, render_template, request, redirect, url_for, send_file
-from openai import OpenAI
+from flask import Flask, render_template, request, redirect, url_for, session, send_file, jsonify, copy_current_request_context
+from flask_executor import Executor
 from urllib.parse import urlparse
 
-from audio import *
 from config import OPENAI_KEY, LOGIN_PASSWORD
+from audio import generate_audio, merge_audio_segments, save_audio_to_temp_file
 from readers import substack, articles
-
+from openai import OpenAI
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
+app.config['EXECUTOR_TYPE'] = 'thread'
+executor = Executor(app)
+
+DEVELOPMENT = True
+
 client = OpenAI(api_key=OPENAI_KEY)
-DEVELOPMENT: bool = True
+
+# Global dictionary to store audio file paths
+audio_file_paths = {}
 
 
 def estimate_processing_time(text) -> float:
-    # Assume each character takes 0.05 seconds to process
     return len(text) * 0.05
 
 
@@ -37,48 +43,54 @@ def login():
 
 @app.route('/home', methods=['GET'])
 def home():
-    return render_template('home.html', estimated_time=None, file_path=None)
+    return render_template('home.html', estimated_time=None)
 
 
-@app.route('/process_audio', methods=['POST'])
-def process_audio():
-    try:
-        url = request.form['url']
-        domain = get_domain(url)
-        logging.info(f"Domain extracted: {domain}")
-
-        if "substack.com" in domain:
-            scraper = substack.SubstackScraper()
-        else:
-            scraper = articles.ArticleReader()
-
-        text = scraper.get_post_content(url)
-        estimated_time = estimate_processing_time(text)
-
-        if DEVELOPMENT is True:
-            temp_file_path = "speech.mp3"
-        else:
-            audio_segments = generate_audio(client, text)
-            merged_audio = merge_audio_segments(audio_segments)
-            temp_file_path = save_audio_to_temp_file(merged_audio)
-
-        logging.info(f"Processing time: {estimated_time}, File: {temp_file_path}")
-
-        return render_template('home.html', estimated_time=estimated_time, file_path=temp_file_path)
-
-    except Exception as e:
-        logging.error(f"Error during POST to /process_audio: {e}")
-        return str(e), 500  # Return the error and a server error status
+@app.route('/check_audio_ready', methods=['GET'])
+def check_audio_ready():
+    if "audio_file_path" in audio_file_paths:
+        file_path = audio_file_paths["audio_file_path"]
+        return jsonify({'is_ready': True, 'file_path': url_for('download_audio', path=file_path)})
+    return jsonify({'is_ready': False})
 
 
-@app.route('/download_audio')
-def download_audio():
-    if file_path := request.args.get('file_path'):
-        return send_file(file_path, mimetype='audio/mpeg', as_attachment=True, download_name='audio.mp3')
+@app.route('/process_article', methods=['POST'])
+def process_article():
+    url = request.form['url']
+    domain = get_domain(url)
+    logging.info(f"Domain extracted: {domain}")
+
+    if "substack.com" in domain:
+        scraper = substack.SubstackScraper()
     else:
-        return "No audio file found.", 404
+        scraper = articles.ArticleReader()
+
+    text = scraper.get_post_content(url)
+    estimated_time = estimate_processing_time(text)
+
+    # Start the audio generation in the background
+    executor.submit(stub_generate_audio, text)
+
+    return render_template('home.html', estimated_time=estimated_time)
+
+
+def stub_generate_audio(text):
+    if DEVELOPMENT:
+        temp_file_path = "speech.mp3"
+    else:
+        audio_segments = generate_audio(client, text)
+        merged_audio = merge_audio_segments(audio_segments)
+        temp_file_path = save_audio_to_temp_file(merged_audio)
+
+    audio_file_paths['audio_file_path'] = temp_file_path  # Save the path for download
+
+
+@app.route('/download_audio', methods=['GET'])
+def download_audio():
+    if file_path := audio_file_paths.get('audio_file_path'):
+        return send_file(file_path, mimetype='audio/mpeg', as_attachment=True, download_name='audio.mp3')
+    return "No audio file found.", 404
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
