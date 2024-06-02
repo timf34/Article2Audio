0e969ecccb862c4027680ec1ff790106eb61730f
timf34 <farrelti@tcd.ie>
2024-06-02 19:11:43 +0100

    adding back config.py as keys are in .env

---
+++ server/config.py
@@ -0,0 +1,15 @@
+"""
+Create a .env file in the root directory of the project and add the following line:
+OPENAI_KEY=your_openai_key
+"""
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_KEY = os.getenv('OPENAI_KEY')
+LOGIN_PASSWORD = os.getenv('LOGIN_PASSWORD')
+SECRET_KEY = os.getenv('SECRET_KEY')
+AUDIO_FILE_NAME = "tmp/audio.mp3"
+LAST_MODIFIED_FILE_NAME = "tmp/last_modified.txt"
+DEVELOPMENT = False
