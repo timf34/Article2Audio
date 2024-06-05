5b0b06ca2361fdd32539837d27720c154528dd84
timf34 <farrelti@tcd.ie>
2024-06-05 20:32:30 +0100

    tried adding tracking again - I don't think adding it to index.html works

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-MP2TQ4TF71');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
