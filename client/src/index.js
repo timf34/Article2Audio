5f8ce8c0163735d525cc0d1f181c6552dfe098b6
timf34 <farrelti@tcd.ie>
2024-06-06 02:09:37 +0100

    google analytics added and react-ga packages removed

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ReactGA from 'react-ga';
-
-ReactGA.initialize('G-MP2TQ4TF71');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
