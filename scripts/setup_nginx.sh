3482573aef0788878f412d8d4862dede7c064169
timf34 <farrelti@tcd.ie>
2024-07-21 18:38:22 +0100

    updated setup_nginx.sh to proxy rss.xml requests to my backend

--- scripts/setup_nginx.sh
+++ scripts/setup_nginx.sh
@@ -47,6 +47,12 @@ server {
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
+
+    location /rss.xml {
+      proxy_pass http://localhost:$BACKEND_PORT/rss.xml;
+      proxy_set_header Host \$host;
+      proxy_set_header X-Real-IP \$remote_addr;
+    }
 }
 EOF
 
