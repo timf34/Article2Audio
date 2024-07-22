baa6ed90abea714f4c076ccbcb37e3012121d42d
timf34 <farrelti@tcd.ie>
2024-07-22 02:35:45 +0100

    using absolute paths

--- server/config.py
+++ server/config.py
@@ -7,11 +7,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 OPENAI_KEY = os.getenv('OPENAI_KEY')
 LOGIN_PASSWORD = os.getenv('LOGIN_PASSWORD')
-MP3_DATA_DIR_PATH = "data/audio_data"
-DATABASE_FILE_PATH = "data/database.db"
-RSS_FILE_PATH = "data/rss.xml"
+MP3_DATA_DIR_PATH = os.path.join(BASE_DIR, "data", "audio_data")
+DATABASE_FILE_PATH = os.path.join(BASE_DIR, "data", "database.db")
+RSS_FILE_PATH = os.path.join(BASE_DIR, "data", "rss.xml")
 
 S3_BUCKET_URL = "https://article2audio.s3.amazonaws.com"
 S3_BUCKET_NAME = "article2audio"
@@ -19,3 +21,4 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', None)
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', None)
 
 DEVELOPMENT = False
+
