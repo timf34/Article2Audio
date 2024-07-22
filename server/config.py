053cd6c99ee30109e26b76cf5d2bf76a1531e74f
timf34 <farrelti@tcd.ie>
2024-07-22 02:24:21 +0100

    removed unused secret key

--- server/config.py
+++ server/config.py
@@ -9,7 +9,6 @@ load_dotenv()
 
 OPENAI_KEY = os.getenv('OPENAI_KEY')
 LOGIN_PASSWORD = os.getenv('LOGIN_PASSWORD')
-SECRET_KEY = os.getenv('SECRET_KEY')
 MP3_DATA_DIR_PATH = "data/audio_data"
 DATABASE_FILE_PATH = "data/database.db"
 RSS_FILE_PATH = "data/rss.xml"
